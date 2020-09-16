e790b2f06988a55a158be7ebe5f0bd7711e1e2b2
Ray <970152657@qq.com>
2020-09-16 09:22:02 +0800

    per frame

--- T_PCL.cpp
+++ T_PCL.cpp
@@ -1,2 +1,32 @@
 #include "T_PCL.h"
 
+int user_data;
+
+//回调-初始化执行一次
+void viewerOneOff(pcl::visualization::PCLVisualizer& viewer)
+{
+    viewer.setBackgroundColor(1.0,0.5,1.0);//设置背景颜色
+    pcl::PointXYZ o;//存储球的圆心位置
+    o.x=1.0;
+    o.y=0;
+    o.z=0;
+    viewer.addSphere(o,0.25,"sphare",0);//添加圆球几何对象
+    std::cout<<"Only run once."<<std::endl;
+}
+
+//回调-每帧显示
+void viewerPsycho(pcl::visualization::PCLVisualizer& viewer)
+{
+    static unsigned count=0;
+    std::stringstream ss;
+    ss<<"Once per viewer loop:"<<count++;
+    viewer.removeShape("text",0);
+    viewer.addText(ss.str(),200,300,"text",0);
+    user_data++;
+}
+
+void test1()
+{
+    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGBA>);
+    // pcl::io::loadPCDFile("")
+}
--- T_PCL.h
+++ T_PCL.h
@@ -1,1 +1,7 @@
 //2020-09-14 Ray
+#include <pcl/visualization/cloud_viewer.h>//类cloudviewer 头文件声名
+#include<iostream>
+#include<pcl/io/io.h>
+#include<pcl/io/pcd_io.h>
+
+
